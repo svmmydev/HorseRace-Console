34f6c6d6e1281e9296b67400d157bc0a99c6d6ab
Roger <belegund@gmail.com>
2024-10-03 10:49:17 +0200

    Changes in comments

--- src/utils/Pause.java
+++ src/utils/Pause.java
@@ -1,2 +1,31 @@
-package utils;public class Pause {
+package utils;
+
+/**
+ * Class created to make dramatic/pragmatic pauses when displaying information through the console.
+ */
+public class Pause {
+
+    /**
+     * Pause the execution of the current thread for the specified number of milliseconds.
+     * Handles InterruptedException appropriately.
+     *
+     * @param value The number of milliseconds to pause the thread.
+     */
+    public static void milliseconds(long value) {
+        try {
+            Thread.sleep(value);
+        } catch (InterruptedException e) {
+            // Leave catch block empty intentionally
+        }
+    }
+
+    /**
+     * Pause the execution of the current thread for the specified number of seconds.
+     * Handles InterruptedException appropriately.
+     *
+     * @param value The number of seconds to pause the thread.
+     */
+    public static void seconds(long value) {
+        milliseconds(value * 1000); // Convert seconds to milliseconds
+    }
 }
