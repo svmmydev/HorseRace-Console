fd530507da1fd6dc9a985ec5bc711ec9a96a949a
Leandro Struni <164394186+LeanEmanuel@users.noreply.github.com>
2024-10-22 20:55:22 +0200

    Translation of Javadoc and variable names in the classes GameHorsesRace, Board, and CardsDeck.

--- src/model/Board.java
+++ src/model/Board.java
@@ -4,11 +4,25 @@ import model.deck.Card;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a game board for the horse racing game.
+ * <p>
+ * The Board is initialized with a specified number of horses and a track length.
+ * It manages the positions of the horses on the track, allowing them to advance
+ * or retreat as the game progresses. It also checks for a winner when a horse
+ * reaches the end of the track.
+ */
 public class Board {
     private Card[][] board;
     private int trackLength;
 
 
+    /**
+     * Construct a Board with the specified horses and track length.
+     *
+     * @param horses The list of horses to place on the board.
+     * @param trackLength The length of the track.
+     */
     public Board(ArrayList<Card> horses, int trackLength) {
         this.trackLength = trackLength;
         this.board = new Card[horses.size()][trackLength];
@@ -16,14 +30,23 @@ public class Board {
     }
 
 
-    //Método para colocar los caballos al principio del tablero
+    /**
+     * Initializes the board by placing the horses at the starting position.
+     *
+     * @param horses The list of horses to be placed on the board.
+     */
     public void initializeBoard(ArrayList<Card> horses) {
         for (int i = 0; i < horses.size(); i++) {
             board[i][0] = horses.get(i);
         }
     }
 
-    public void retrocederCaballo(int row) {
+    /**
+     * Retreats the horse in the specified row by moving it one position backward.
+     *
+     * @param row The row of the horse to be retreated.
+     */
+    public void retreatHorse(int row) {
         for (int col = 0; col < trackLength - 1; col++) {
             if (board[row][col] != null) {
                 board[row][col - 1] = board[row][col];
@@ -33,9 +56,12 @@ public class Board {
         }
     }
 
-    //TODO agregar la metodo otra parámetro +1 o -1 que indique cuantas posiciones avanzar o retroceder
-    //Mètodo para hacer avanzar o retroceder caballo
-    public void avanzarCaballo(int row) {
+    /**
+     * Advances the horse in the specified row by moving it ine position forward.
+     *
+     * @param row The row of the horse to be advanced.
+     */
+    public void advanceHorse(int row) {
         for (int col = 0; col < trackLength - 1; col++) {
             if (board[row][col] != null) {
                 board[row][col + 1] = board[row][col];
@@ -45,21 +71,34 @@ public class Board {
         }
     }
 
-    //Método para comprobar el estado del juego, si hay ganador
+    /**
+     * Check the state of the game to see if there is a winner.
+     *
+     * @return The winning card if there is a winner, null otherwise
+     */
     public Card checkWinner() {
-        for (int row = 0; row < board.length; row++) {
-            if (board[row][trackLength - 1] != null) {
-                return board[row][trackLength - 1];
+        for (Card[] cards : board) {
+            if (cards[trackLength - 1] != null) {
+                return cards[trackLength - 1];
             }
         }
         return null;
     }
 
+    /**
+     * Gets the current state of the board
+     *
+     * @return A 2D array representing the board.
+     */
     public Card[][] getBoard() {
         return board;
     }
 
-
+    /**
+     * Gets the length of the track.
+     *
+     * @return The length of the track.
+     */
     public int getTrackLength() {
         return trackLength;
     }
--- src/model/deck/CardsDeck.java
+++ src/model/deck/CardsDeck.java
@@ -2,20 +2,25 @@ package model.deck;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a deck of cards for the game.
+ * This class is responsible for creating a deck of cards,
+ * managing the cards, and providing methods to draw and remove cards.
+ */
 public class CardsDeck {
     private ArrayList<Card> cardsDeck = new ArrayList<>(); // The Deck himself
     private int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // The numbers who use to create Deck
     private CardSuit[] cardSuits = {CardSuit.GOLD, CardSuit.CLUBS, CardSuit.CUPS, CardSuit.SWORDS}; // The suits to create Deck
     private CardFace[] cardFaces = {CardFace.JACK, CardFace.KNIGHT, CardFace.KING}; // Figures/face to create Deck
-    private Card card; //  Object to safe a Card temporarily to use after
-    private ArrayList<Integer> numCartes; //  To store the index of card who has been distribute yet
+    private ArrayList<Integer> cardNumber; //  To store the index of card who has been distributed yet
 
     /**
      * Constructor to create the cards deck every time you want to play
      * Create Cards from two vectors (number and suit) and add them to the deck ArrayList
      */
-
     public CardsDeck() {
+        //  Object to safe a Card temporarily to use after
+        Card card;
         for (int i = 0; i < num.length; i++) {
             for (int j = 0; j < cardSuits.length; j++) {
                 card = new NumeredCard(num[i], cardSuits[j]);
@@ -28,36 +33,49 @@ public class CardsDeck {
                 cardsDeck.add(card);
             }
         }
-
-        numCartes = new ArrayList<>();
+        cardNumber = new ArrayList<>();
     }
 
+
     /**
-     * Método para devolver el tamaño del mazo de cartas
+     * Returns the size of the card deck.
      *
-     * @return Devuelve el tamaño del ArrayList de cartas
+     * @return The size of the cards deck.
      */
     public int getDeckSize() {
         return cardsDeck.size();
     }
 
 
+    /**
+     * Returns the array of card faces.
+     *
+     * @return An array of card faces.
+     */
     public CardFace[] getCardFaces() {
         return cardFaces;
     }
 
+
+    /**
+     * Removes the specified card from the deck.
+     *
+     * @param card The card to be removed.
+     */
     public void removeCard(Card card) {
         cardsDeck.remove(card);
     }
 
 
-    //Metodo para agregar los caballos al array de cartas que ya salieron
-    public void setNumCartes(ArrayList<Integer> numCartes) {
-        this.numCartes = numCartes;
+    /**
+     * Sets the indices of the cards that have already been distributed.
+     *
+     * @param cardNumber The list of indices of cards that have been distributed.
+     */
+    public void setCardNumber(ArrayList<Integer> cardNumber) {
+        this.cardNumber = cardNumber;
     }
 
-
-    //TODO realizar bucle < numero de cartas del mazo, devuelve carta que saco el crupier
 
     /**
      * Method Return a card from the card deck
@@ -65,43 +83,53 @@ public class CardsDeck {
      * @return A card  from the deck
      */
     public Card getCardFromDeck() {
-        Card cartadonada;
-        int numcarta = comprovarNumCartes();
+        Card drawCard;
+        int cardIndex = checkDistributedCardNumbers();
 
-        cartadonada = cardsDeck.get(numcarta);
-        return cartadonada;
+        drawCard = cardsDeck.get(cardIndex);
+        return drawCard;
     }
 
 
     /**
      * Helper method that returns a random card from Deck that has not been given before
      *
-     * @return A int number that represent the index from the deck
+     * @return AN int number that represent the index from the deck
      */
-    private int comprovarNumCartes() {
+    private int checkDistributedCardNumbers() {
         boolean found;
-        int numcarta;
+        int cardNumber;
         do {
             found = false;
-            numcarta = (int) (Math.random() * cardsDeck.size());
-            if (!numCartes.isEmpty()) {
-                for (Integer x : numCartes) {
-                    if (numcarta == x) {
+            cardNumber = (int) (Math.random() * cardsDeck.size());
+            if (!this.cardNumber.isEmpty()) {
+                for (Integer x : this.cardNumber) {
+                    if (cardNumber == x) {
                         found = true;
                         break;
                     }
                 }
             }
         } while (found);
-        numCartes.add(numcarta);
-        return numcarta;
+        this.cardNumber.add(cardNumber);
+        return cardNumber;
     }
 
-    //Metodo que devuelve la carta del indice indicado
+
+    /**
+     * Returns the card at the specified index.
+     *
+     * @param i The index of the card.
+     * @return The card at the specified index.
+     */
     public Card getCardAt(int i) {
         return cardsDeck.get(i);
     }
 
+
+    /**
+     * Prints all cards in the deck to the console.
+     */
     public void getCardsDeck() {
         for (Card i: cardsDeck){
             System.out.println(i);
