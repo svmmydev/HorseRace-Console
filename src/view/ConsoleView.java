6008e239ab42dbe9866c637b985618626d08b6f2
Roger <belegund@gmail.com>
2024-10-24 12:10:42 +0200

    Implement displayWelcomeMessage and getHumanNames to ConsoleView class. Also minor changes to Main

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -4,8 +4,10 @@ import model.Board;
 import model.deck.Card;
 import model.player.Human;
 import model.player.Player;
+import utils.ConsoleInOut;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class ConsoleView {
 
@@ -13,11 +15,51 @@ public class ConsoleView {
     public static void displayRaceWinner(Card winner) { // TODO
     }
 
-    public void displayWelcomeMessage() { //TODO
+    /**
+     * Displays the welcome message through console when starting the app
+     */
+    public void displayWelcomeMessage() {
+        System.out.println("Welcome to the Horses Race betting game!");
     }
 
-    public String[] getHumanNames(int minHumans, int maxPlayers) { // TODO
-        return null;
+    /**
+     * Asks for user input to know the amount of human players that will participate in the game and their names.
+     * The amount is limited by minHumans and maxPlayers.
+     * Uses a HashSet to avoid repeated human names.
+     * @param minHumans minimum amount of human players that the game needs
+     * @param maxPlayers maximum amount of players (in this case humans) that the game accepts
+     * @return String[]: an array with the name of the human players that will be participating
+     */
+    public String[] getHumanNames(int minHumans, int maxPlayers) {
+        final String NAME_REGEXP = "[0-9A-Za-z._]{4,15}";
+        HashSet<String> nameSet = new HashSet<>(); // To check for repeated human names
+        int humanPlayersAmount = ConsoleInOut.getIntegerInRange(
+                "How many human players will be playing?\n" +
+                        "Minimum "+minHumans+" maximum "+maxPlayers+".",
+                minHumans,
+                maxPlayers
+        );
+        String[] humanNames = new String[humanPlayersAmount];
+        String name;
+
+        for (int i=0; i<humanPlayersAmount; i++) {
+
+            do {
+                name = ConsoleInOut.getStringWithRegex(
+                        "Enter human player " + (i + 1) + " of " + humanPlayersAmount + " name:\n" +
+                                "\"Length 4-15, letters, numbers, \\\".\\\" and \\\"_\\\"\"",
+                        NAME_REGEXP
+                );
+                if (!nameSet.contains(name.toLowerCase())) {
+                    humanNames[i] = name;
+                    nameSet.add(name.toLowerCase());
+                    break;
+                }
+                System.out.println("\""+name+"\" has already been chosen, chose another name.");
+            } while (true);
+        }
+
+        return humanNames;
     }
     public int getNumberOfBots(int minBots, int maxBots) { // TODO
         return 0;
