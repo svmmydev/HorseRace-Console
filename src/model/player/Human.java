1afbbb2fdc3aa96f1ea5ac8f3c13dc48d194ad56
Sammy <svmmydev@gmail.com>
2024-11-01 00:18:10 +0100

    Ajuste del bankroll

--- src/model/player/Human.java
+++ src/model/player/Human.java
@@ -2,7 +2,7 @@ package model.player;
 
 public class Human extends Player {
     public Human(String userName, int bankroll) {
-        super(userName, 20);
+        super(userName, bankroll);
     }
 
 }
